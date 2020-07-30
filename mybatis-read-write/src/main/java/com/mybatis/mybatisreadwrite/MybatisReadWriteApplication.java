aaeb68f61a3c1aa601c98536bdd746f9a28212a4
wangpp <987897856@qq.com>
2020-07-30 19:54:24 +0800

    read-write init

---
+++ mybatis-read-write/src/main/java/com/mybatis/mybatisreadwrite/MybatisReadWriteApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis.mybatisreadwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisReadWriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisReadWriteApplication.class, args);
+    }
+
+}

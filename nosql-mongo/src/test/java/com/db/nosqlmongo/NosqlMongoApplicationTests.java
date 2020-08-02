331d7cf2530258530794256bacbbfca9ae1e78e3
tianwamg <987897856@qq.com>
2020-08-02 14:17:23 +0800

    nosql init

---
+++ nosql-mongo/src/test/java/com/db/nosqlmongo/NosqlMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.nosqlmongo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ nosql-redis/src/test/java/com/db/nosqlredis/NosqlRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.nosqlredis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
